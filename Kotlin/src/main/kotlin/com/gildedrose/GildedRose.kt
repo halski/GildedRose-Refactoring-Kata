7d66f8d4b9e3cd3a9317ebd47874456f5ab4c7ad
Jakub Michalski <halski@gmail.com>
2025-05-25 22:35:05 +0200

    working specification covering existing code

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,12 +1,18 @@
 package com.gildedrose
 
+const val BACKSTAGE_PASSES_ITEM_NAME = "Backstage passes to a TAFKAL80ETC concert"
+const val SULFURAS_ITEM_NAME = "Sulfuras, Hand of Ragnaros"
+const val BRIE_ITEM_NAME = "Aged Brie"
+const val CONJURED_ITEM_NAME = "Conjured Mana Cake"
+
 class GildedRose(val items: List<Item>) {
 
     fun updateQuality() {
+
         for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (items[i].name != BRIE_ITEM_NAME && items[i].name != BACKSTAGE_PASSES_ITEM_NAME) {
                 if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                    if (items[i].name != SULFURAS_ITEM_NAME) {
                         items[i].quality = items[i].quality - 1
                     }
                 }
@@ -14,7 +20,7 @@ class GildedRose(val items: List<Item>) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (items[i].name == BACKSTAGE_PASSES_ITEM_NAME) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1
@@ -30,15 +36,15 @@ class GildedRose(val items: List<Item>) {
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+            if (items[i].name != SULFURAS_ITEM_NAME) {
                 items[i].sellIn = items[i].sellIn - 1
             }
 
             if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (items[i].name != BRIE_ITEM_NAME) {
+                    if (items[i].name != BACKSTAGE_PASSES_ITEM_NAME) {
                         if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                            if (items[i].name != SULFURAS_ITEM_NAME) {
                                 items[i].quality = items[i].quality - 1
                             }
                         }
@@ -55,4 +61,3 @@ class GildedRose(val items: List<Item>) {
     }
 
 }
-
