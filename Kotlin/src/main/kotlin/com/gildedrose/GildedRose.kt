6efab48e20cb75056c17e235bce31a6d4b040556
Jakub Michalski <halski@gmail.com>
2025-05-26 09:51:59 +0200

    refactor - further reduce around name, combine whens; move invariant checks onto Item

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -8,50 +8,69 @@ const val CONJURED_ITEM_NAME = "Conjured Mana Cake"
 class GildedRose(private val items: List<Item>) {
 
     fun updateItems() {
-        for (item in items) {
-            item.update()
-        }
+        items.forEach { it.update() }
     }
 }
 
 fun Item.update() {
+    updateSellIn()
+    updateQuality()
+}
 
-    when (name) {
-        BRIE_ITEM_NAME -> if (quality < 50) standardQualityIncrease()
-        BACKSTAGE_PASSES_ITEM_NAME -> {
-            if (quality < 50) {
-                standardQualityIncrease()
-                if (sellIn < 11) standardQualityIncrease()
-                if (sellIn < 6) standardQualityIncrease()
-            }
-        }
-        SULFURAS_ITEM_NAME -> {}
-        else -> if (quality > 0) standardQualityDecrease()
-    }
-
+private fun Item.updateSellIn() {
     when (name) {
         SULFURAS_ITEM_NAME -> {}
         else -> advanceByADay()
     }
+}
 
-    if (sellIn < 0) {
-        when (name) {
-            BRIE_ITEM_NAME -> if (quality < 50) standardQualityIncrease()
-            BACKSTAGE_PASSES_ITEM_NAME -> quality = 0
-            SULFURAS_ITEM_NAME -> {}
-            else -> if (quality > 0) standardQualityDecrease()
+private fun Item.updateQuality() {
+    when (name) {
+        BRIE_ITEM_NAME -> {
+            val increaseBy = if (isExpired()) 2 else 1
+            increaseQuality(increaseBy)
+        }
+
+        BACKSTAGE_PASSES_ITEM_NAME -> {
+            if (isExpired()) {
+                decreaseQualityToZero()
+            } else {
+                val increaseBy = when {
+                    sellIn < 5 -> 3
+                    sellIn < 10 -> 2
+                    else -> 1
+                }
+                increaseQuality(increaseBy)
+            }
+        }
+
+        SULFURAS_ITEM_NAME -> {}
+
+        else -> {
+            val decreaseBy = if (isExpired()) 2 else 1
+            decreaseQuality(decreaseBy)
         }
     }
 }
 
-private fun Item.standardQualityDecrease() {
-    quality -= 1
+private const val MAX_QUALITY = 50
+
+private fun Item.isExpired() = sellIn < 0
+
+private fun Item.decreaseQuality(decreaseBy: Int = 1) {
+    if (quality - decreaseBy < 0) quality = 0
+    else quality -= decreaseBy
 }
 
-private fun Item.standardQualityIncrease() {
-    quality += 1
+private fun Item.increaseQuality(increaseBy: Int = 1) {
+    if (quality + increaseBy > MAX_QUALITY) quality = MAX_QUALITY
+    else quality += increaseBy
 }
 
 private fun Item.advanceByADay() {
     sellIn -= 1
+}
+
+private fun Item.decreaseQualityToZero() {
+    quality = 0
 }
